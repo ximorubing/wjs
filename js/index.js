fe19acdf31ec5718d8d5e0d4252647a6902e662c
ximorubing <taoxitong@sina.com>
2019-09-17 06:39:40 +0000

    finish banner

--- js/index.js
+++ js/index.js
@@ -1,11 +1,77 @@
 $(function(){
-    $.ajax({
-        url:"js/data.json",
-        dataType:"json",
-        success:function(data){
-            console.log(data);
-        }
-    })
-
     
+banner();
+
 });
+
+var banner=function(){
+
+    var getData=function(callback){
+
+        if(window.data){
+            callback&&callback(window.data);
+        }else{
+        $.ajax({
+            url:"js/data.json",
+            dataType:"json",
+            data:'',
+            success:function(data){
+                window.data=data;
+                callback&&callback(window.data);
+            }
+        })
+    }
+    };
+
+    var render=function(){
+        getData(function(data){
+            var isMobile=$(window).width()>768?true:false;
+            //console.log(isMobile);
+            var pointHtml=template("pointtp2",{list:data});
+            var imageHtml=template("imagetp1",{list:data,isMobile:isMobile})
+            //console.log({list:data,isMobile:isMobile});
+            //console.log(pointHtml);
+            //console.log(imageHtml);
+            $(".carousel-indicators").html(pointHtml);
+            $(".carousel-inner").html(imageHtml);
+        });
+    }
+
+    $(window).on("resize",function(){
+        render();
+    }).trigger("resize");
+    
+    var isMove=false;
+    var distanceX=0;
+    var startX=0;
+    $(".banner").on("touchstart",function(e){
+        
+        startX=e.originalEvent.touches[0].clientX;
+        //console.log(startX);
+    }).on("touchmove",function(e){
+        //console.log(e);
+        var moveX=e.originalEvent.touches[0].clientX;
+        //console.log(moveX);
+        distanceX=moveX-startX;
+        
+        isMove=true;
+
+    }).on("touchend",function(e){
+        //console.log(distanceX);
+        if(isMove&&Math.abs(distanceX)>50){
+            
+            if(distanceX<0){
+                $(".carousel").carousel("next");
+
+            }else{
+                $(".carousel").carousel("prev");
+
+            }
+        }
+        isMove=false;
+        distanceX=0;
+        startX=0;
+
+
+    });
+}
